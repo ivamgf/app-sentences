3a8432f24fc7684498d69793be16b8cb7c78677f
Ivam Galvão Filho <programador.ivam@gmail.com>
2023-11-09 22:11:30 -0300

    first commit

---
+++ application/src/app/modules/recovery/recovery.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recovery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recovery.component.html',
+  styleUrl: './recovery.component.scss'
+})
+export class RecoveryComponent {
+
+}
